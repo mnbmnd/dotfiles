5fb0d3fa19744eb8d760e8ed3b319fa793072885
Muneeb Mennad <mennadmuneeb@gmail.com>
2026-02-07 22:14:27 -0330

    Moved files into subdirs

--- terminal/wezterm/.wezterm.lua
+++ terminal/wezterm/.wezterm.lua
@@ -16,12 +16,12 @@ config.color_scheme = 'Gruvbox Dark (Gogh)'
 
 config.window_frame = {
   -- The font used in the tab bar.
-  -- Roboto Bold is the default; this font is bundled
-  -- with wezterm.
   -- Whatever font is selected here, it will have the
   -- main font setting appended to it to pick up any
   -- fallback fonts you may have used there.
-  font = wezterm.font { family = 'Roboto', weight = 'Bold' },
+ 
+  
+
 
   -- The size of the font in the tab bar.
   -- Default to 10.0 on Windows but 12.0 on other systems
@@ -95,6 +95,21 @@ config.window_background_opacity = 1.0
 
 -- Text background opacity
 config.text_background_opacity = 0.5
+
+-- Spawn a fish shell in login mode
+-- config.default_prog = { '/usr/local/bin/fish', '-l' }
+
+-- Launching a different program as a one off via the CLI
+-- wezterm start -- vim ~/.wezterm.lua
+config.font = wezterm.font 'Fira Code'
+-- You can specify some parameters to influence the font selection;
+-- for example, this selects a Bold, Italic font variant.
+config.font =
+  wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
+
+-- Key Binding
+
+
 
 -- Finally, return the configuration to wezterm:
 return config
