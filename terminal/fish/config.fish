6b26f31b14111192adfa1d8f508d728d0d40f33d
Muneeb Mennad <mennadmuneeb@gmail.com>
2026-02-08 09:43:58 -0330

    Update system setup

--- terminal/fish/config.fish
+++
@@ -1,9 +0,0 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-set -x LANG en_CA.UTF-8
-set -x LC_ALL en_CA.UTF-8
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
-fish_add_path ~/.local/bin
