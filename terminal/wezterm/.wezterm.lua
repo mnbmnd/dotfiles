7034b806ec9126ed893abbe10e817b3058949c5b
Muneeb Mennad <mennadmuneeb@gmail.com>
2026-02-07 23:09:21 -0330

    Add tinty

--- terminal/wezterm/.wezterm.lua
+++ terminal/wezterm/.wezterm.lua
@@ -1,10 +1,23 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
+
+local function read_file(path)
+	local f = io.open(path, "r")
+	if not f then
+		return nil
+	end
+	local s = f:read("*l")
+	f:close()
+	return s
+end
+
+local scheme = read_file(os.getenv("HOME") .. "/.local/share/tinted-theming/tinty/artifacts/current_scheme")
+-- The config for wezterm
 
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
@@ -12,58 +25,55 @@ config.initial_rows = 28
 
 -- or, changing the font size and color scheme.
 config.font_size = 10
-config.color_scheme = 'Gruvbox Dark (Gogh)'
+config.color_scheme = "Gruvbox dark, medium (base16)"
 
 config.window_frame = {
-  -- The font used in the tab bar.
-  -- Whatever font is selected here, it will have the
-  -- main font setting appended to it to pick up any
-  -- fallback fonts you may have used there.
- 
-  
+	-- The font used in the tab bar.
+	-- Whatever font is selected here, it will have the
+	-- main font setting appended to it to pick up any
+	-- fallback fonts you may have used there.
 
+	-- The size of the font in the tab bar.
+	-- Default to 10.0 on Windows but 12.0 on other systems
+	font_size = 9.0,
 
-  -- The size of the font in the tab bar.
-  -- Default to 10.0 on Windows but 12.0 on other systems
-  font_size = 9.0,
+	-- The overall background color of the tab bar when
+	-- the window is focused
+	active_titlebar_bg = "#333333",
 
-  -- The overall background color of the tab bar when
-  -- the window is focused
-  active_titlebar_bg = '#333333',
-
-  -- The overall background color of the tab bar when
-  -- the window is not focused
-  inactive_titlebar_bg = '#333333',
+	-- The overall background color of the tab bar when
+	-- the window is not focused
+	inactive_titlebar_bg = "#333333",
 }
 
 config.colors = {
-  tab_bar = {
-    -- The color of the inactive tab bar edge/divider
-    inactive_tab_edge = '#575757',
-  },
+	tab_bar = {
+		-- The color of the inactive tab bar edge/divider
+		inactive_tab_edge = "#575757",
+	},
 }
 
 -- Window padding styling
 config.window_padding = {
-  left = 3,
-  right = 3,
-  top = 0,
-  bottom = 1,
+	left = 3,
+	right = 3,
+	top = 0,
+	bottom = 1,
 }
 
 -- Inactive pane styling
 config.inactive_pane_hsb = {
-  saturation = 0.9,
-  brightness = 0.6,
+	saturation = 0.9,
+	brightness = 0.6,
 }
 
 -- Attach an image to the background of the wezterm window, uncomment below.
 -- config.window_background_image = '/path/to/wallpaper.jpg'
 
 -- config.window_background_image_hsb = {
-  -- brightness = 0.3,
-  -- hue = 1.0,
-  -- saturation = 1.0,
+-- brightness = 0.3,
+-- hue = 1.0,
+-- saturation = 1.0,
 -- }
 
 -- config.window_background_gradient = {
@@ -101,15 +111,12 @@ config.text_background_opacity = 0.5
 
 -- Launching a different program as a one off via the CLI
 -- wezterm start -- vim ~/.wezterm.lua
-config.font = wezterm.font 'Fira Code'
+config.font = wezterm.font("Fira Code")
 -- You can specify some parameters to influence the font selection;
 -- for example, this selects a Bold, Italic font variant.
-config.font =
-  wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
+config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true })
 
 -- Key Binding
-
-
 
 -- Finally, return the configuration to wezterm:
 return config
